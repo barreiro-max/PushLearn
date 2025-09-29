0a51b0c72753fe47470a950a48e19f7bb63a9762
Max <maxtop4k022@gmail>
2025-09-29 13:06:17 +0300

    Initial Commit

---
+++ PushLearn/PushLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushLearnApp.swift
+//  PushLearn
+//
+//  Created by admin on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct PushLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PushLearnTests/PushLearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  PushLearnTests.swift
+//  PushLearnTests
+//
+//  Created by admin on 29.09.2025.
+//
+
+import Testing
+@testable import PushLearn
+
+struct PushLearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
