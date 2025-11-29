9cf0b4e17729ad985b2e5cdb0937e21dd99bcaef
Max <barreiro.fontela.max@gmail.com>
2025-11-29 15:18:45 +0200

    [Feature] Add LanguageAvailability module and extension for languages; remove unnecessary extension and ViewModel

---
+++ PushLearn/Core/Translation/LanguageAvailability/Languages.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension Locale.Language {
+    static let en: Self = .init(identifier: "en")
+    static let fr: Self = .init(identifier: "fr")
+    static let de: Self = .init(identifier: "de")
+    static let es: Self = .init(identifier: "es")
+    static let uk: Self = .init(identifier: "uk")
+}
