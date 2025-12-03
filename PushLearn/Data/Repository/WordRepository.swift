779da79bf396a8ab72782fb5302b4a8240f57614
Max <barreiro.fontela.max@gmail.com>
2025-12-03 19:22:30 +0200

    [Refactor] Update WordRepository to use RemoteDataSource

--- PushLearn/Data/Repository/WordRepository.swift
+++ PushLearn/Data/Repository/WordRepository.swift
@@ -1,9 +1,31 @@
+import FirebaseFirestore
+
+protocol WordRepositoryProtocool {
+    
+}
+
 struct WordRepository: WordRepositoryProtocool {
-    private let database: any Firestored
+    private let database: any Database
     
     init(
-        database: some Firestored
+        database: some Database
     ) {
         self.database = database
+    }
+    
+    public func add(word: Word) {
+
+    }
+
+    public func fetchWords() -> [Word] {
+        []
+    }
+    
+    public func update(wordId: String) {
+        
+    }
+    
+    public func delete(wordId: String) {
+        
     }
 }
