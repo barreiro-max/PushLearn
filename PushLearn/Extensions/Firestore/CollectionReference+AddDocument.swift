57528c9b6416cbf22dbfd9eab5d619e71b0108f2
Max <barreiro.fontela.max@gmail.com>
2025-12-05 00:33:27 +0200

    [Feature] Add async/await support for Firestrore method addDocument(from:) using continuation

--- PushLearn/Extensions/Firestore/CollectionReference+AddDocument.swift
+++ PushLearn/Extensions/Firestore/CollectionReference+AddDocument.swift
@@ -4,9 +4,19 @@ extension CollectionReference {
     func addDocument<T: Encodable>(
         from value: T,
         encoder: Firestore.Encoder = Firestore.Encoder()
-    ) async throws -> DocumentReference {
-        try await withCheckedThrowingContinuation { continuation in
-            
+    ) async throws {
+        return try await withCheckedThrowingContinuation { continuation in
+            do {
+                try self.addDocument(from: value, encoder: encoder) { error in
+                    if let error {
+                        continuation.resume(throwing: error)
+                    } else {
+                        continuation.resume()
+                    }
+                }
+            } catch {
+                continuation.resume(throwing: error)
+            }
         }
     }
 }
