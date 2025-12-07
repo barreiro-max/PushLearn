5b0e795d68070e83186cf426543755f193296a45
Max <barreiro.fontela.max@gmail.com>
2025-12-07 16:02:12 +0200

    [Refactor] Extract UI components in AuthFields

--- PushLearn/Features/Auth/Views/Custom/AuthFields.swift
+++ PushLearn/Features/Auth/Views/Custom/AuthFields.swift
@@ -6,10 +6,18 @@ struct AuthFields: View {
     @Binding var state: AuthState
     
     var body: some View {
+        authEmailField
+        authSecureField
+    }
+    
+    private var authEmailField: some View {
         AuthEmailField(
             email: $email,
             state: $state
         )
+    }
+    
+    private var authSecureField: some View {
         AuthSecureField(
             password: $password,
             state: $state
