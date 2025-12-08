219f8756dea95857c7994823e42fb961f06c0414
Max <barreiro.fontela.max@gmail.com>
2025-12-09 01:09:39 +0200

    [Feature] Add ErrorView to ModernDictionaryView and integrate into UI components

--- PushLearn/Features/Dictionary/Modern/ModernDictionaryView.swift
+++ PushLearn/Features/Dictionary/Modern/ModernDictionaryView.swift
@@ -15,22 +15,52 @@ struct ModernDictionaryView: View {
     ]
     
     var body: some View {
+        if isEmptyDictionary {
+            ContentUnavailableView(
+                "Пуш нові слова!",
+                systemImage: "tray.and.arrow.down"
+            )
+        } else {
+            dictionary
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    LinearGradient(
+                        colors: background,
+                        startPoint: .top,
+                        endPoint: .bottom
+                    ),
+                    ignoresSafeAreaEdges: .top
+                )
+                .onChangeConfiguration(with: dictVM)
+                .translateTextOnPage(with: dictVM)
+        }
+    }
+    
+    private var isEmptyDictionary: Bool {
+        dictVM.sources.isEmpty && dictVM.targets.isEmpty
+    }
+    
+    private var dictionary: some View {
         List {
-            ForEach(dictVM.source, id: \.self) { sourceUnit in
-                Text(sourceUnit)
+            if let error = dictVM.errorTranslationMessage {
+                ErrorView(globalError: error)
+            } else {
+                sourcesView
+                targetsView
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(
-            LinearGradient(
-                colors: background,
-                startPoint: .top,
-                endPoint: .bottom
-            ),
-            ignoresSafeAreaEdges: .top
-        )
-        .onChangeConfiguration(with: dictVM)
-        .translateTextOnPage(with: dictVM)
+    }
+    
+    private var sourcesView: some View {
+        ForEach(dictVM.sources, id: \.self) { initialWord in
+            Text(initialWord)
+        }
+    }
+    
+    private var targetsView: some View {
+        ForEach(dictVM.targets, id: \.self) { translatedWord in
+            Text(translatedWord)
+        }
     }
 }
 
