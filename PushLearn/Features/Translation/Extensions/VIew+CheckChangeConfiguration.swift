7e8315c9aa9b5b0cf6d0895da748a4c156c4771c
Max <barreiro.fontela.max@gmail.com>
2025-12-12 20:40:14 +0200

    [Refactor] Clean up onChangeConfiguration and translateTextOnPage functions in View extension

--- PushLearn/Features/Translation/Extensions/VIew+CheckChangeConfiguration.swift
+++ PushLearn/Features/Translation/Extensions/VIew+CheckChangeConfiguration.swift
@@ -3,12 +3,7 @@ extension View {
     @available(iOS 18.0, *)
     func onChangeConfiguration(with viewModel: ModernDictionaryVM) -> some View {
         onAppear {
-            guard viewModel.configuration == nil
-            else {
-                viewModel.rebuildConfiguration()
-                return
-            }
-            viewModel.triggerTranslation()
+            viewModel.prepareOrRebuildConfiguration()
         }
     }
 }
--- PushLearn/Features/Translation/Extensions/View+TranslationTask.swift
+++ PushLearn/Features/Translation/Extensions/View+TranslationTask.swift
@@ -5,7 +5,7 @@ extension View {
     @available(iOS, introduced: 18.0)
     func translateTextOnPage(with viewModel: ModernDictionaryVM) -> some View {
         translationTask(viewModel.configuration) { session in
-            await viewModel.translateAll(using: session)
+            await viewModel.translateAllSources(using: session)
         }
     }
 }
