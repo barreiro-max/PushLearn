17f77fa4fbb293cfe92f07dab5e9b471cdba58a6
Max <barreiro.fontela.max@gmail.com>
2025-12-12 20:12:54 +0200

    [Refactor] Improve ForgetPassword viewmodel

--- PushLearn/Features/Auth/ForgetPw/ForgetPasswordVM.swift
+++ PushLearn/Features/Auth/ForgetPw/ForgetPasswordVM.swift
@@ -1,21 +1,13 @@
 import Foundation
 import FirebaseAuth
 
-@Observable
-public class ForgetPasswordVM {
-    private var authValidator: any AuthValidated
-    
+@MainActor
+@Observable final public class ForgetPasswordVM {
     var state: AuthState = .idle
-    
-    init(
-        authValidator: some AuthValidated = AuthValidator()
-    ) {
-        self.authValidator = authValidator
-    }
-    
+        
     func resetPassword(email: String) {
         state = .idle
-        Task {
+        Task { @MainActor in
             do {
                 try await Auth.auth().sendPasswordReset(withEmail: email)
             } catch {
