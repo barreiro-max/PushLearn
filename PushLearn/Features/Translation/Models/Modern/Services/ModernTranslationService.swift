15fd34f2587e478f6a4da2249099685fd8a20f2e
Max <barreiro.fontela.max@gmail.com>
2025-12-12 20:34:11 +0200

    [Feature] Add ModernTranslationService with Translating protocol

---
+++ PushLearn/Features/Translation/Models/Modern/Services/ModernTranslationService.swift
@@ -0,0 +1,18 @@
+import Translation
+
+@available(iOS 18.0, *)
+protocol Translating: Translation {
+    func translate(for words: [Word], using session: TranslationSession) async throws -> [TranslationSession.Response]
+}
+
+@available(iOS, introduced: 18.0)
+public struct ModernTranslationService: Translating {
+    func translate(for words: [Word], using session: TranslationSession) async throws -> [TranslationSession.Response] {
+        let requests: [TranslationSession.Request] = words.map {
+            TranslationSession.Request(sourceText: $0.source)
+        }
+        let responses = try await session.translations(from: requests)
+        return responses
+    }
+}
+
