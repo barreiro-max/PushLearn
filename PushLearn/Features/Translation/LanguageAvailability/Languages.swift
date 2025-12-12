725192c487717e72a64701f8c4e97f7141a03337
Max <barreiro.fontela.max@gmail.com>
2025-12-12 20:02:00 +0200

    [Refactor] Remove Languages extension

--- PushLearn/Features/Translation/LanguageAvailability/Languages.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Locale.Language {
-    static let en: Self = .init(identifier: "en")
-    static let fr: Self = .init(identifier: "fr")
-    static let de: Self = .init(identifier: "de")
-    static let es: Self = .init(identifier: "es")
-    static let uk: Self = .init(identifier: "uk")
-}
