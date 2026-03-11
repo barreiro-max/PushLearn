baf2c2caa5ff248b871f65efc0f0a6e9659814ce
Max <barreiro.fontela.max@gmail.com>
2026-03-11 09:41:16 +0200

    [Refactor] Add DI to the LanguageStore

--- PushLearn/Features/Translation/Models/Services/LanguageStore.swift
+++ PushLearn/Features/Translation/Models/Services/LanguageStore.swift
@@ -8,14 +8,20 @@ struct LanguageStore: LanguageStoreSettings {
     private let sourceKey = "sourceLanguage"
     private let targetkey = "selectedLanguage"
     
+    private let defaults: UserDefaults
+    
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+    
     var source: Locale.Language {
         let preferedLanguage = Locale.preferredLanguages[0].prefix(2)
-        let value = UserDefaults.standard.string(forKey: sourceKey) ?? String(preferedLanguage)
+        let value = defaults.string(forKey: sourceKey) ?? String(preferedLanguage)
         return .init(identifier: value)
     }
     
     var target: Locale.Language {
-        let value = UserDefaults.standard.string(forKey: targetkey) ?? "en"
+        let value = defaults.string(forKey: targetkey) ?? "en"
         return .init(identifier: value)
     }
 }
