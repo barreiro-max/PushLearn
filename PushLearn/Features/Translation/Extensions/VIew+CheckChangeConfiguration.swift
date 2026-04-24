88e5c6bf63008e49accce66a05fd218c473c495c
Max <barreiro.fontela.max@gmail.com>
2026-04-24 17:07:51 +0300

    [Refactor] Remove @available attribute from View extensions

--- PushLearn/Features/Translation/Extensions/VIew+CheckChangeConfiguration.swift
+++ PushLearn/Features/Translation/Extensions/VIew+CheckChangeConfiguration.swift
@@ -1,6 +1,7 @@
 import SwiftUI
+
 extension View {
-    @available(iOS 18.0, *)
+
     func onChangeConfiguration(with viewModel: DictionaryVM) -> some View {
         onAppear {
             viewModel.prepareOrRebuildConfiguration()
--- PushLearn/Features/Translation/Extensions/View+TranslationTask.swift
+++ PushLearn/Features/Translation/Extensions/View+TranslationTask.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import Translation
 
 extension View {
-    @available(iOS, introduced: 18.0)
+
     func translateTextOnPage(with viewModel: DictionaryVM) -> some View {
         translationTask(viewModel.configuration) { session in
             await viewModel.translateAllSources(using: session)
