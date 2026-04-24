a0868d6d83ad103dca58b0b94f1be1936198f51e
Max <barreiro.fontela.max@gmail.com>
2026-04-24 20:37:24 +0300

    [Refactor] Improve UserProfile models

--- PushLearn/Features/Auth/Models/UserProfile/UserProfile.swift
+++ PushLearn/Features/Auth/Models/UserProfile/UserProfile.swift
@@ -1,13 +1,17 @@
-struct UserProfile {
+struct UserProfile: Sendable {
+
     var id: String?
     var username: String?
     var email: String?
 }
 
 extension UserProfile {
+
     init(dto: UserProfileDTO) {
         self.id = dto.id
         self.username = dto.username
         self.email = dto.email
     }
 }
+
+extension UserProfile: Equatable {}
--- PushLearn/Features/Auth/Models/UserProfile/UserProfileDTO.swift
+++ PushLearn/Features/Auth/Models/UserProfile/UserProfileDTO.swift
@@ -1,11 +1,14 @@
-import FirebaseFirestore
+@preconcurrency import FirebaseFirestore
+
 struct UserProfileDTO: Codable {
+
     @DocumentID var id: String?
     var username: String?
     var email: String?
 }
 
 extension UserProfileDTO {
+
     init(domain: UserProfile) {
         self.id = domain.id
         self.username = domain.username
