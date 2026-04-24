8a1f90c976c7653a1000fa98c9c7402393191a17
Max <barreiro.fontela.max@gmail.com>
2026-04-24 20:36:58 +0300

    [Refactor] Improve AuthValidator
    - Follow DRY principle
    - Conform AuthValidated to Sendable

--- PushLearn/Features/Auth/Models/Validators/AuthValidator.swift
+++ PushLearn/Features/Auth/Models/Validators/AuthValidator.swift
@@ -1,37 +1,48 @@
 import Foundation
 
-protocol AuthValidated {
+protocol AuthValidated: Sendable {
+
     func isEmailValid(_ email: String) -> Bool
     func isPasswordValid(_ password: String) -> Bool
     func getValidationState(email: String, password: String) -> AuthState
 }
 
 struct AuthValidator: AuthValidated {
-    // MARK: - Validation methods
+
     func isEmailValid(_ email: String) -> Bool {
-        let emailRegexPattern = "(?i)^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$"
-        let regex = try? Regex(emailRegexPattern)
-        let res = try? regex?.wholeMatch(in: email)
-        return res != nil
+        matches(
+            regexPattern: "(?i)^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$",
+            in: email
+        )
     }
 
     func isPasswordValid(_ password: String) -> Bool {
-        let pwRegexPattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
-        let regex = try? Regex(pwRegexPattern)
-        let res = try? regex?.wholeMatch(in: password)
-        return res != nil
+        matches(
+            regexPattern: "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            in: password
+        )
+    }
+
+    private func matches(regexPattern: String, in string: String) -> Bool {
+        guard let regex = try? Regex(regexPattern) else {
+            return false
+        }
+
+        return (try? regex.wholeMatch(in: string)) != nil
     }
 }
 
 extension AuthValidator {
-    // MARK: - State Management
+
     func getValidationState(email: String, password: String) -> AuthState {
         guard isEmailValid(email) else {
             return .failure(email: "Невалідна електронна пошта")
         }
+
         guard isPasswordValid(password) else {
             return .failure(password: "Невалідний пароль")
         }
-        return .validationSuccess
+
+        return .validated
     }
 }
